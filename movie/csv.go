4fa0c419e1626cfe7f76e3f3c78a95d347e8b8dc
Yihao Wang <yesiamyihao@gmail.com>
2024-09-01 00:30:35 +1200

    Refactor

--- movie/csv.go
+++ movie/csv.go
@@ -9,10 +9,10 @@ import (
 
 // Entries returns a sequence of entries from a CSV file.
 // The first line is assumed to be header and is dropped.
-func Entries(r io.Reader) (seq iter.Seq2[[]string, error]) {
+func Entries(r io.Reader) iter.Seq2[[]string, error] {
 	reader := csv.NewReader(r)
 	_, _ = reader.Read() // discard the first line
-	seq = func(yield func([]string, error) bool) {
+	return func(yield func([]string, error) bool) {
 		for {
 			entry, err := reader.Read()
 			if errors.Is(err, io.EOF) || !yield(entry, err) {
@@ -20,5 +20,4 @@ func Entries(r io.Reader) (seq iter.Seq2[[]string, error]) {
 			}
 		}
 	}
-	return
 }
